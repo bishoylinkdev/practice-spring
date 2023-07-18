9a05461f336b0ca2940ab0f72efd6f0338ad4a3f
Bishoy Adly <bishoy.adly@linkdev.com>
2023-07-18 21:29:48 +0300

    Add home page HTML

--- src/main/java/com/example/practicespring/controllers/HomeController.java
+++ src/main/java/com/example/practicespring/controllers/HomeController.java
@@ -3,9 +3,11 @@ package com.example.practicespring.controllers;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 @Slf4j
@@ -18,8 +20,15 @@ public class HomeController {
         private String password;
     }
 
+    @GetMapping("/")
+    public ModelAndView homeEndpoint() {
+        ModelAndView modelAndView = new ModelAndView("index");
+        modelAndView.addObject("message", "Spring Boot Application");
+        return modelAndView;
+    }
+
     @PostMapping("/login")
-    public void homeEndpoint(@RequestBody LoginDto loginDto) {
+    public void loginEndpoint(@RequestBody LoginDto loginDto) {
         log.info("login username : {}", loginDto.getUsername());
         log.info("login password : {}", loginDto.getPassword());
     }
